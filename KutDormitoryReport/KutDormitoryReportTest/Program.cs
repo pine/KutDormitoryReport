1bdc4face53e8b1b7bf5f324e2d27b2039895805
MIZUNE Pine <pinemz@gmail.com>
2014-04-18 21:10:30 +0900

    Initial commit

---
+++ KutDormitoryReport/KutDormitoryReportTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RobotClub.DormitoryReport
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var data = new DormitoryReportData();
+            data.Name = "宮永 咲";
+            
+            data.RoomNo = "A150";
+            data.PhoneNumber = "090-9999-9999";
+            data.LeaderName = "竹井 久";
+            data.LeaderPhoneNumber = "090-0000-0000";
+            data.Reason = "麻雀部の活動のため";
+
+            var form = DormitoryReport.CreateWindow();
+
+            form.SetData(data);
+            Application.Run(form);
+        }
+    }
+}
